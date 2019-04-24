ddc899d84cc0d350d82627a14fd6707fa6b72382
Egor Ogdanets <ogdanets2010@yandex.ru>
2019-04-24 21:35:44 +0300

    Add Tree to part2

---
+++ Lesson6DZ/src/part2/Node.java
@@ -0,0 +1,11 @@
+package part2;
+
+public class Node {
+    public int x;
+    public Node leftChild;
+    public Node rightChild;
+
+    public void display() {
+        System.out.println("X :" + x);
+    }
+}
