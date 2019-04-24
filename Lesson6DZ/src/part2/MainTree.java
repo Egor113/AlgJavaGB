f7105e351b260834fb2c2c75ff94599d9047175d
Egor Ogdanets <ogdanets2010@yandex.ru>
2019-04-24 23:20:11 +0300

    Add comments to Main

--- Lesson6DZ/src/part2/MainTree.java
+++ Lesson6DZ/src/part2/MainTree.java
@@ -1,35 +1,52 @@
 package part2;
 
-import part1.Tree;
-
 import java.util.ArrayList;
 import java.util.Random;
 
 public class MainTree {
     public static void main(String[] args) {
-        Tree tree = new Tree();
-        ArrayList<ArrayList> treeList = new ArrayList<>();
+        //Создадим список деревьев
+        ArrayList<Tree> treeList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            treeList.add(new Tree());
+        }
+
+        //Создадим список списков из целых чисел
+        ArrayList<ArrayList> intList = new ArrayList<>();
+
+        //Заполним списки случайными числами
         for (int i = 0; i < 10; i++) {
             ArrayList<Integer> list = new ArrayList<>();
             Random rnd = new Random();
             for (int j = 0; j < 10; j++) {
                 list.add(-100 + rnd.nextInt(200));
             }
-            treeList.add(list);
+            intList.add(list);
         }
 
-        for (int i = 0; i < treeList.size(); i++) {
-            for (int j = 0; j < treeList.get(i).size(); j++) {
-                System.out.print(treeList.get(i).get(j) + " ");
+        System.out.println("Исходные массивы чисел: ");
+        for (int i = 0; i < intList.size(); i++) {
+            for (int j = 0; j < intList.get(i).size(); j++) {
+                System.out.print(intList.get(i).get(j) + " ");
             }
             System.out.println();
         }
+        System.out.println();
+
+
+        //Добавим элементы в список деревьев
+        for (int i = 0; i < intList.size(); i++) {
+            for (int j = 0; j < intList.get(i).size(); j++) {
+                treeList.get(i).insert((int)intList.get(i).get(j));
+            }
+        }
+
 
         for (int i = 0; i < treeList.size(); i++) {
-            for (int j = 0; j < treeList.get(i).size(); j++) {
-                System.out.print(treeList.get(i).get(j) + " ");
-            }
+            System.out.println("Дерево №" + i);
+            treeList.get(i).display();
             System.out.println();
         }
+
     }
 }
