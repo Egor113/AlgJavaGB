12cef59ec1fc302c8945dab909dc6d6e572667e4
Egor Ogdanets <ogdanets2010@yandex.ru>
2019-04-28 12:41:57 +0300

    Add lesson7

---
+++ Lesson7DZ/src/part1/GraphApp.java
@@ -0,0 +1,18 @@
+package part1;
+
+public class GraphApp {
+    public static void main(String[] args) {
+        Graph graph = new Graph();
+        graph.addVertex('A');
+        graph.addVertex('B');
+        graph.addVertex('C');
+        graph.addVertex('D');
+        graph.addVertex('E');
+        graph.addEdge(0, 1); // A - B
+        graph.addEdge(1, 2); // B - C
+        graph.addEdge(0, 3); // A - D
+        graph.addEdge(3, 4); // D - E
+        //graph.dfs();
+        graph.bfs();
+    }
+}
