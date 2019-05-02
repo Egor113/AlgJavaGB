9d2cb1269a387ef3ed3a4f1d8067c32ca3657f22
Egor Ogdanets <ogdanets2010@yandex.ru>
2019-05-02 15:37:03 +0300

    Fixed add method

--- Lesson8DZ/src/lesson8DZ/HashTableChain.java
+++ Lesson8DZ/src/lesson8DZ/HashTableChain.java
@@ -22,46 +22,57 @@ public class HashTableChain {
     public void insert(ChainItem item) {
         int key = item.getKey();
         int hashVal = hashFunc(key);
-        int secondHashVal = secondHashFunc(key);
-        while (hashArray[hashVal] != null && hashArray[hashVal].getKey() != -1) {
-            hashVal += secondHashVal;
-            hashVal %= sizeArr;
+        hashVal %= sizeArr;
+        if (hashArray[hashVal] == null) {
+            hashArray[hashVal] = new ChainItemList();
         }
-        hashArray[hashVal] = item;
-    }
-
-    public ChainItem find(int key) {
-        int hashVal = hashFunc(key);
-        int secondHashVal = secondHashFunc(key);
-        while (hashArray[hashVal] != null) {
-            if (hashArray[hashVal].getKey() == key) {
-                return hashArray[hashVal];
-            }
-            hashVal += secondHashVal;
-            hashVal %= sizeArr;
-        }
-        return null;
-    }
-
-    public ChainItem delete(int key) {
-        int hashVal = hashFunc(key);
-        int secondHashVal = secondHashFunc(key);
-        while (hashArray[hashVal] != null) {
-            if (hashArray[hashVal].getKey() == key) {
-                ChainItem temp = hashArray[hashVal];
-                hashArray[hashVal] = nonItem;
-                return temp;
-            }
-            hashVal += secondHashVal;
-            hashVal %= sizeArr;
-        }
-        return null;
+        hashArray[hashVal].addItem(item);
     }
 
     public void display() {
         for (int i = 0; i < sizeArr; i++) {
-            if (hashArray[i] != null) System.out.println(hashArray[i].getKey());
+            if (hashArray[i] != null){
+                for (int j = 0; j < hashArray[i].getList().size(); j++) {
+                    System.out.print(hashArray[i].getList().get(j).getKey() + " ");
+                }
+                System.out.println();
+            }
             else System.out.println("element is null");
         }
     }
+
+//    public ChainItem find(int key) {
+//        int hashVal = hashFunc(key);
+//        int secondHashVal = secondHashFunc(key);
+//        while (hashArray[hashVal] != null) {
+//            if (hashArray[hashVal].getKey() == key) {
+//                return hashArray[hashVal];
+//            }
+//            hashVal += secondHashVal;
+//            hashVal %= sizeArr;
+//        }
+//        return null;
+//    }
+//
+//    public ChainItem delete(int key) {
+//        int hashVal = hashFunc(key);
+//        int secondHashVal = secondHashFunc(key);
+//        while (hashArray[hashVal] != null) {
+//            if (hashArray[hashVal].getKey() == key) {
+//                ChainItem temp = hashArray[hashVal];
+//                hashArray[hashVal] = nonItem;
+//                return temp;
+//            }
+//            hashVal += secondHashVal;
+//            hashVal %= sizeArr;
+//        }
+//        return null;
+//    }
+//
+//    public void display() {
+//        for (int i = 0; i < sizeArr; i++) {
+//            if (hashArray[i] != null) System.out.println(hashArray[i].getKey());
+//            else System.out.println("element is null");
+//        }
+//    }
 }
